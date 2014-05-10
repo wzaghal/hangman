9e6c48e2ca8db97951e7bc690f7a99849518715c
wzaghal <wisamzaghal@gmail.com>
2014-05-10 18:38:57 -0400

    improve layout and javascript functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
+'use strict';
+
 $(document).ready(function(){
-	var answer = 'fawaz';
 	var chances = 6;
 	var isDone = false;
 	var correctKeys = 0;
@@ -11,35 +12,34 @@ $(document).ready(function(){
 	var pushToList = function(keyCode){
 		wrongLetters.push(keyCode);
 		var letter= String.fromCharCode(keyCode);
-		$("#wrong-letters").append('<li> ' + letter + '</li>');
+		$('#wrong-letters').append('<li> ' + letter + '</li>');
 
-	}
+	};
 
 
 	$(document).keyup(function(e) {
 
-		if(chances > 0 && !isDone){
-
+		if (chances > 0 && !isDone){
 			
 			
 			//Right letter	
-			if( e.keyCode == letters[0]){ //f
-				$(".letter1").removeClass("hide-text");
+			if( e.keyCode === letters[0]){ //f
+				$('.letter1').removeClass('hide-text');
 				correctKeys++;
 			}
-			else if (e.keyCode == letters[1]){ //a
-				$(".letter2").removeClass("hide-text");
-				$(".letter4").removeClass("hide-text");
+			else if (e.keyCode === letters[1]){ //a
+				$('.letter2').removeClass('hide-text');
+				$('.letter4').removeClass('hide-text');
 				correctKeys++;
 				correctKeys++;
 
-			}else if (e.keyCode == letters[2]){ //w
-				$(".letter3").removeClass("hide-text");
+			}else if (e.keyCode === letters[2]){ //w
+				$('.letter3').removeClass('hide-text');
 				correctKeys++;
 
 			}
-			else if (e.keyCode == letters[3]){ //z
-				$(".letter5").removeClass("hide-text");
+			else if (e.keyCode === letters[3]){ //z
+				$('.letter5').removeClass('hide-text');
 				correctKeys++;
 
 			}
@@ -47,10 +47,10 @@ $(document).ready(function(){
 
 
 			//possibly a wrong character
-			else if ( letters.indexOf(e.keyCode) == -1 ){
+			else if ( letters.indexOf(e.keyCode) === -1 ){
 
 				//definitely wrong character
-				if (wrongLetters.indexOf(e.keyCode) == -1 ){
+				if (wrongLetters.indexOf(e.keyCode) === -1 ){
 
 					//check if letter already used
 					if (wrongLetters.indexOf(e.keyCode) >= 0){
@@ -62,39 +62,47 @@ $(document).ready(function(){
 						pushToList(e.keyCode);
 						
 
-						if (chances==6){
-							$(".head").removeClass("hide");
-						}else if (chances==5){
-							$(".body").removeClass("hide");
-						}else if (chances==4){
-							$(".left-arm").removeClass("hide");
-						}else if (chances==3){
-							$(".right-arm").removeClass("hide");
+						if (chances===6){
+							$('.head').removeClass('hide');
+						}else if (chances===5){
+							$('.body').removeClass('hide');
+						}else if (chances===4){
+							$('.leftArm').removeClass('hide');
+						}else if (chances===3){
+							$('.rightArm').removeClass('hide');
 							//addClass
-						}else if (chances==2){
-							$(".right-leg").removeClass("hide");
-						}else if (chances==1){
-							$(".left-leg").removeClass("hide");
+						}else if (chances===2){
+							$('.leftLeg').removeClass('hide');
+						}else if (chances===1){
+							$('.rightLeg').removeClass('hide');
 						}
-						chances--;	
+						chances--;
 					}
 					
 				}
 				
 			}
-			if (correctKeys == 5)
+
+			if (correctKeys === 5){
 				isDone=true;
+			}
 
 
 			
 		}
-		else if (chances == 0){
+		
+	if (chances === 0){
+		console.log('test');
+		$('.fail').removeClass('hide');
+	}else if (isDone){
+		console.log('test');
+		$('.success').removeClass('hide');
+	}});
 
-			$(".fail").removeClass("hide");
-		}else{
-			$(".success").removeClass("hide");
-		}
-	});
+
+	
+
+
 
 });
 
